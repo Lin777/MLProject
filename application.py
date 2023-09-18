a3039b4381f5a4a1c1992a5a65c0fa4d9f240fb2
lin777 <evelyn.cusi@synfioo.com>
2023-09-18 19:30:15 +0200

    Fix predict_datapoint method of pipeline to deploy

--- application.py
+++ application.py
@@ -35,7 +35,7 @@ def predict_datapoint():
         
         predict_pipeline=PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        print(results[0])
+        return render_template('home.html', results=results[0])
     
 if __name__=="__main__":
     app.run(host="0.0.0.0")#, debug=True)
